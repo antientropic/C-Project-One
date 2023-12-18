19b49a7a3b3a4bd254884f3b3b2b5a6e7b221f2b
antientropic <154105338+antientropic@users.noreply.github.com>
2023-12-17 21:11:53 -0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 /* This program compares the following five sort algorithms:
  * Heapsort, mergesort, quick sort, select sort, and insert sort
  * Author: Clifford Anderson
